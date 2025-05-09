efeb8b81a785a3a676dc20d94cb16c9183f78cb6
richard lyman <richard.r.lyman@gmail.com>
2025-05-08 22:38:31 -0700

    enable plots and change time to gettime(0)

--- PGHI_test.py
+++ PGHI_test.py
@@ -45,7 +45,7 @@ def sweep_test():
           
 def audio_test():
     for nfile in range(100): # 100 arbitrary file limit
-        etime = time.clock()        
+        etime = time.clock_gettime(0)  
         song_title, audio_in = p.plt.get_song()
         if audio_in is None: 
             break          
@@ -60,7 +60,7 @@ def audio_test():
         p.test( song_title)
 #         saved = p.setverbose(True)          
         p.plt.signal_to_file(np.stack(stereo), song_title, override_verbose = True) 
-        p.logprint('elapsed time = {:8.2f} seconds\n'.format(time.clock()- etime))
+        p.logprint('elapsed time = {:8.2f} seconds\n'.format(time,time.clock_gettime(0)- etime))
         p.setverbose(saved)        
         
 def warble_test():
@@ -88,20 +88,20 @@ p = pghi.PGHI(tol = 1e-3, show_frames = 100, time_scale=1/scale_up, freq_scale=s
 # gamma =gl**2*.25645
 # p = pghi.PGHI(tol = 1e-6, show_plots = False, show_frames=10, g=g,gamma = gamma, gl=gl)
 
-# p.setverbose(False)
+p.setverbose(True)
 warble_test()
 pulse_test()
 sine_test()
 sweep_test()
-p.setverbose(False)    
+# p.setverbose(False)    
 audio_test()
 
-p = rtpghi.PGHI(tol = 1e-3, show_frames = 100, time_scale=1/scale_up, freq_scale=scale_up, show_plots = False, verbose=True)
+p = rtpghi.PGHI(tol = 1e-3, show_frames = 100, time_scale=1/scale_up, freq_scale=scale_up, show_plots = True, verbose=True)
 
-# warble_test()
-# pulse_test()
-# sine_test()
-# sweep_test()
+warble_test()
+pulse_test()
+sine_test()
+sweep_test()
 p.setverbose(False)    
 audio_test()
 
